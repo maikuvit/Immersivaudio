121da840c77f6bd683d4803d45633116ba587a02
danieleavolio <daniele.avolio14@gmail.com>
2024-05-06 11:14:44 +0100

    Other tests

--- Code/Scripts/frame_extractor.py
+++ Code/Scripts/frame_extractor.py
@@ -1,26 +1,23 @@
-# Using ffmpeg
-# Extract 2 frames each seconds of a video
-# save the frames in a folder with the same name as the video and an id
-
 import os
 import subprocess
 import sys
 
-def extract_frames(video_path, output_path):
+def extract_frames(video_path, output_path, fps):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
     video_name = os.path.basename(video_path)
     output_folder = os.path.join(output_path, video_name)
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    command = "ffmpeg -i " + video_path + " -vf fps=2 " + output_folder + "/%d.jpg"
+    command = f"ffmpeg -i {video_path} -vf fps={fps} {output_folder}/%d.jpg"
     subprocess.call(command, shell=True)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python frame_extractor.py <video_path> <output_path>")
+    if len(sys.argv) != 4:
+        print("Usage: python frame_extractor.py <video_path> <output_path> <fps>")
         sys.exit(1)
     video_path = sys.argv[1]
     output_path = sys.argv[2]
-    extract_frames(video_path, output_path)
+    fps = sys.argv[3]
+    extract_frames(video_path, output_path, fps)
     print("Frames extracted successfully")
