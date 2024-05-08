ff09bd4360faee472410e4fd68e2c5333d75d719
maikuvit <michelevitale000@gmail.com>
2024-05-08 11:34:55 +0100

    updated frame_extractor module

--- Code/Scripts/frame_extractor.py
+++ Code/Scripts/frame_extractor.py
@@ -1,6 +1,9 @@
+import math
 import os
 import subprocess
 import sys
+import cv2 as cv
+
 
 def extract_frames(video_path, output_path, fps):
     if not os.path.exists(output_path):
@@ -9,15 +12,33 @@ def extract_frames(video_path, output_path, fps):
     output_folder = os.path.join(output_path, video_name)
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
-    command = f"ffmpeg -i {video_path} -vf fps={fps} {output_folder}/%d.jpg"
+    command = f"ffmpeg -i {video_path} -vf fps={fps} {output_folder}/%d.jpg -hide_banner -loglevel error"
     subprocess.call(command, shell=True)
+
+def fps_count(seconds, factor):
+    return math.floor(math.log(seconds,10) * factor)
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python frame_extractor.py <video_path> <output_path> <fps>")
+        print("Usage: python frame_extractor.py <video_path> <output_path> <fps factor>")
         sys.exit(1)
     video_path = sys.argv[1]
     output_path = sys.argv[2]
-    fps = sys.argv[3]
+    factor = (int) (sys.argv[3])
+
+    video = cv.VideoCapture(video_path)
+
+    fps = video.get(cv.CAP_PROP_FPS)
+    frame_count = video.get(cv.CAP_PROP_FRAME_COUNT)
+
+    sec = (int) (frame_count / fps) 
+
+    frames = fps_count( sec, factor)
+    fps = math.floor(frames / sec * 100) / 100 # floor to two decimal places
+
+    print(f"Total extracted frames: {frames}\nSeconds video duration: {sec}\nFPS: {fps}")    
     extract_frames(video_path, output_path, fps)
     print("Frames extracted successfully")
+
+
+
