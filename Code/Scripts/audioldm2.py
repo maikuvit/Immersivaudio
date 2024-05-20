3331af8aa13136dcbf29fe85b15f3c1463ab6e1c
danieleavolio <daniele.avolio14@gmail.com>
2024-05-20 11:58:18 +0100

    Changes to work faster

--- Code/Scripts/audioldm2.py
+++ Code/Scripts/audioldm2.py
@@ -3,12 +3,15 @@ import torch
 from diffusers import AudioLDM2Pipeline
 import json
 import os
+from diffusers import DPMSolverMultistepScheduler
 
 repo_id = "cvssp/audioldm2"
+
 
 def audo_generate(input_json):
     input_json = json.loads(input_json)
     prompt = input_json["description"]
+    negative_prompt = "Low quality, average quality, bad quality, poor quality, terrible quality, bad audio"
     video_id = input_json["video_id"]
 
     pipe = AudioLDM2Pipeline.from_pretrained(repo_id, torch_dtype=torch.float16)
@@ -17,15 +20,18 @@ def audo_generate(input_json):
 
         # set the seed for generator
         generator = torch.Generator("cuda").manual_seed(0)
+        pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
+        pipe.enable_model_cpu_offload()
 
     # run the generation
     audio = pipe(
         prompt,
-        num_inference_steps=200,
-        audio_length_in_s=10.0,
-        num_waveforms_per_prompt=3,
-        generator=generator,
-    ).audios
+        negative_prompt=negative_prompt,
+        num_waveforms_per_prompt=1,
+        audio_length_in_s=20,
+        num_inference_steps=50,
+        generator=generator.manual_seed(0),
+    ).audios[0]
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
     audio_path = os.path.join(dir_path, "output/audio")
