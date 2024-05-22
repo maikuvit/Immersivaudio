5fbc53edcc46fda32ef2b4ac93077d41f82bae22
danieleavolio <daniele.avolio14@gmail.com>
2024-05-22 14:43:27 +0100

    Gradio for image and videos

--- Code/Scripts/gradio_interface.py
+++ Code/Scripts/gradio_interface.py
@@ -4,15 +4,18 @@ import gradio as gr
 
 from main import main
 
+def main(video_path):
+    print(video_path)
+
 
 # Define the interface components and structure them.
-demo = gr.Interface(
+video_interface = gr.Interface(
     fn=main,
     inputs=[
         gr.Video(label="Video Path Input")  # Input component
     ],
     outputs=[
-        gr.Video(label="Processed Video", interactive=False),       # Video output+
+        gr.Video(label="Processed Video", interactive=False),       # Video output
         gr.Audio(label="Generated audio", interactive=False),
         gr.Text(label="Used prompt", interactive = False),
         gr.Text(label="Json complete output", interactive=False), 
@@ -20,4 +23,27 @@ demo = gr.Interface(
     allow_flagging="never"
 )
 
+image_interface = gr.Interface(
+    fn=main,
+    inputs=[
+        gr.Image(label="Image Input")  # Input component
+    ],
+    outputs=[
+        gr.Image(label="Processed Video", interactive=False),       # Video output
+        gr.Text(label="Generated audio", interactive=False),
+        gr.Text(label="Used prompt", interactive = False),
+        gr.Text(label="Json complete output", interactive=False), 
+    ],
+    allow_flagging="never"
+)
+
+# Use both interfaces
+demo = gr.TabbedInterface(
+    [
+        video_interface, image_interface
+    ],
+    tab_names=["Video", "Image"]
+)
+
+# Launch the interface
 demo.launch(share=True)
