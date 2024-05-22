3db218d1bd7038705a26e1a0d17bca6391fe6f9e
danieleavolio <daniele.avolio14@gmail.com>
2024-05-22 11:45:22 +0100

    combiner update

--- Code/Scripts/prompt_combiner.py
+++ Code/Scripts/prompt_combiner.py
@@ -1,13 +1,24 @@
-
-def recombine_prompt(description_json,object_json):
+def recombine_prompt(description_json, object_json, music=True):
 
     description_json.update(object_json)
 
-    description_json.update({"prompt_combiner":{
-        "prompt" : "Melodic music based on " + 
-        description_json["frame_description"]["description"] +
-        "Considering the keywords: " + 
-        ",".join(description_json["object_detection"]["total_keywords"])
-    }})
-
-    return description_json
+    if music:
+        description_json.update(
+            {
+                "prompt_combiner": {
+                    "prompt": "Melodic music based on "
+                    + description_json["frame_description"]["description"] 
+                }
+            }
+        )
+    else:
+        description_json.update(
+            {
+                "prompt_combiner": {
+                    "prompt": "Sound effects based on "
+                    + description_json["frame_description"]["description"]
+                    + " with the following keywords: "
+                    + ",".join(description_json["object_detection"]["total_keywords"])
+                }
+            }
+        )
